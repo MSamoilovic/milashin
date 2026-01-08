3917ef5d545971efaf9660867823e4c48ed7dd42
MSamoilovic <msamoilovic@gmail.com>
2026-01-08 18:18:40 +0100

    started work on milashin agent

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+import argparse
+
+from dotenv import load_dotenv
+from google import genai
+
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    client = genai.Client(api_key=api_key)
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+   
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
+
+    if response.usage_metadata is None:
+        raise ValueError("something is wrong!")
+    
+    print(f"User Prompt: Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
+    print(response.text)
+
+
+
+
+if __name__ == "__main__":
+    main()
