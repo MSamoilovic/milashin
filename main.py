f3623e2daedcc8b30084998ed5e973e0758c6568
MSamoilovic <msamoilovic@gmail.com>
2026-01-09 17:50:15 +0100

    Added Calculator

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import argparse
 
 from dotenv import load_dotenv
 from google import genai
-
-
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -14,16 +13,20 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
    
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
 
     if response.usage_metadata is None:
         raise ValueError("something is wrong!")
     
-    print(f"User Prompt: Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose == True:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Response:")
     print(response.text)
 
