da3dffd510ec2bda986a93d3affe951f34bfa5cf
MSamoilovic <msamoilovic@gmail.com>
2026-01-14 12:25:45 +0100

    feat(agent): Add functionality to call functions with LLMw

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 MAX_CHARACTER_LIMIT = 10000
+WORKING_DIR = "./calculator"
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import argparse
 
 from dotenv import load_dotenv
@@ -6,7 +7,7 @@ from google import genai
 from google.genai import types
 
 from prompts import system_prompt
-from functions.call_function import available_functions
+from functions.call_function import available_functions, call_function
 
 def main():
     load_dotenv()
@@ -21,33 +22,57 @@ def main():
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
    
-    response = client.models.generate_content(
-        model="gemini-2.5-flash", 
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools =[available_functions],
-            system_instruction=system_prompt,
-            temperature=0
-        ),
-    )
+    for iteration in range(20):
+        response = client.models.generate_content(
+            model="gemini-2.5-flash", 
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions],
+                system_instruction=system_prompt,
+                temperature=0
+            ),
+        )
 
-    if response.usage_metadata is None:
-        raise ValueError("something is wrong!")
+        if response.usage_metadata is None:
+            raise ValueError("Response metadata is missing!")
+
     
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+
+        if args.verbose:
+            print(f"--- Iteration {iteration + 1} ---")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     
-    print("Response:")
+        calls = response.function_calls
 
-    calls = response.function_calls
+        if calls:
+            function_result_parts = []
 
-    if calls:
-        for function_call in calls:
-            print(f"Calling function: {function_call.name}({function_call.args})")
-    else:
-        print(response.text)
+            for function_call in calls:
+                function_call_result = call_function(function_call, args.verbose)
+
+                if not function_call_result.parts:
+                    raise Exception("Parts list is empty")
+                
+               
+                function_result_parts.append(function_call_result.parts[0])
+
+                if args.verbose:
+                    result_data = function_call_result.parts[0].function_response.response
+                    print(f"Function Result [{function_call.name}]: {result_data.get('result')}")
+
+            messages.append(types.Content(role="user", parts=function_result_parts))
+
+        else:
+            print("\nResponse:")
+            print(response.text)
+            return  
+   
+    print(f"Error: Agent failed to reach a conclusion within 20 iterations.")
+    sys.exit(1)
 
 
 
